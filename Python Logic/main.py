81b730c665ed83626497376d91b9de7876503c09
EinHades <kramertom964@gmail.com>
2026-02-08 20:55:03 -0500

    Switch to using Ollama's Python client instead of AirLLM for Hardware compatibiltity and ease of use.

--- Python Logic/main.py
+++ Python Logic/main.py
@@ -1,30 +1,12 @@
-from airllm import AutoModel
+from ollama import chat
+from ollama import ChatResponse
 
-MAX_LENGTH = 128
-# could use hugging face model repo id:
-
-# or use model's local path...
-#model = AutoModel.from_pretrained("/home/ubuntu/.cache/huggingface/hub/models--garage-bAInd--Platypus2-70B-instruct/snapshots/b585e74bcaae02e52665d9ac6d23f4d0dbc81a0f")
-
-print("What do you want to know? ")
-user_input = input()
-input_text = [user_input]
-
-model = AutoModel.from_pretrained("google/gemma-3-4b-it")
-
-input_tokens = model.tokenizer(input_text,
-    return_tensors="pt", 
-    return_attention_mask=False, 
-    truncation=True, 
-    max_length=MAX_LENGTH, 
-    padding=False)
-           
-generation_output = model.generate(
-    input_tokens['input_ids'].cuda(), 
-    max_new_tokens=20,
-    use_cache=True,
-    return_dict_in_generate=True)
-
-output = model.tokenizer.decode(generation_output.sequences[0])
-
-print(output)
+response: ChatResponse = chat(model='gemma3', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
+# or access fields directly from the response object
+print(response.message.content)
