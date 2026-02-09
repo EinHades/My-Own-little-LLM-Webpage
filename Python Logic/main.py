9c5427fae6dbc2405eff2efdbc112de9f674b430
EinHades <kramertom964@gmail.com>
2026-02-09 17:56:10 -0500

    Added Stlye.css ,removed / moved the mess I made last night with WebStorm.

--- Python Logic/main.py
+++ Python Logic/main.py
@@ -1,12 +1,15 @@
 from ollama import chat
 from ollama import ChatResponse
 
-response: ChatResponse = chat(model='gemma3', messages=[
+print("Ask a question to the model:")
+user_input = input()
+
+response: ChatResponse = chat(model='gemma3:270m', messages=[
   {
     'role': 'user',
-    'content': 'Why is the sky blue?',
+    'content': user_input,
   },
 ])
-print(response['message']['content'])
+#print(response['message']['content'])
 # or access fields directly from the response object
 print(response.message.content)
